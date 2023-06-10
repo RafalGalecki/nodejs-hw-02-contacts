9378ba4420065b9c8a0515a8b31125c8449aa3c9
Rafał Gałecki <darthunter@gmail.com>
2023-06-10 17:33:23 +0200

    connection works

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+
+// console.log(process.env.DB_HOST);
+
+const UriDb = process.env.DB_HOST;
+const DB_HOST =
+  "mongodb+srv://darthunter:Kielbasa_21@cluster0.nlj5opc.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", false);
+
+const connection = mongoose.connect(DB_HOST, {
+  useUnifiedTopology: true,
+  // useFindAndModify: true,
+});
+
+connection
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((err) => {
+    console.log(`Server error: ${err.message}`);
+    process.exit();
+  });
