1144df1f78ddeeccaabd04b85dc0bc178609b966
Rafał Gałecki <darthunter@gmail.com>
2023-06-10 20:56:32 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controller/index");
 
-router.get("/", controller.get);
+router.get("/", controller.getAll);
 
-router.get("/:id", controller.getById);
+router.get("/:id", controller.get);
 
-router.delete("/:id", controller.removeById);
+router.delete("/:id", controller.remove);
 
 router.post("/", controller.create);
 
